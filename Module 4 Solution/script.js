adeda21b449d9cfd3303f7eb4be51e9016a42314
RAEX86 <divyanshumahato63@gmail.com>
2023-05-07 22:31:05 +0530

    mod4

--- Module 4 Solution/script.js
+++ Module 4 Solution/script.js
@@ -1,21 +1,14 @@
-var names=new Array();
-names[0]="Yuvraj";
-names[1]="John";
-names[2]="Jimmy";
-names[3]="jason";
-names[4]="payal";
-names[5]="faruk";
-names[6]="lara";
-names[7]="priya";
-names[8]="linda";
-names[9]="jack";
 
+(function (window) {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i = 0; i < names.length; i++) {
-	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
-        console.log("Goodbye "+ names[i])
-	}
-	else{
-		console.log("Hello "+ names[i])
-	}
-}
+  for (var i in names) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})(window);
+
